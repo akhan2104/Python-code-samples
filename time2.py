ea8e155c5ff743ad154551b3dc7eb6331aa61c72
akhan2104 <123791161+akhan2104@users.noreply.github.com>
2023-02-10 00:55:23 -0600

    Add files via upload

---
+++ time2.py
@@ -0,0 +1,9 @@
+str_time = input("What time is it now?")
+str_wait_time = input("What is the number of Hours to wait?")
+time = int(str_time)
+wait_time = int(str_wait_time)
+
+time_when_alarm_go_off = time + wait_time
+print(time_when_alarm_go_off)
+
+#Adnan khan
